566c229197f6c2c1aacc29d4925349cdd47f4763
unknown <joakig6@gmail.com>
2023-02-27 11:09:42 -0300

    css testing

--- src/Components/Services/ServicesEN.jsx
+++ src/Components/Services/ServicesEN.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
-import lightstyles from './Services.module.css'
-import darkstyles from './ServicesW.module.css'
+import lightstyles from './ServicesW.module.css'
+import darkstyles from './Services.module.css'
 import imgfrontserv1 from '../../assets/Frontservices1.png'
+import imgfrontserv2 from '../../assets/Frontservices2.png'
+import imgfrontserv3 from '../../assets/Frontservices3.png'
+import imgfrontserv4 from '../../assets/Frontservices4.png'
+import imgfrontserv5 from '../../assets/Frontservices5.png'
 export default function ServicesEN({darkmode}){
     const style= darkmode?darkstyles:lightstyles
     const [actualsection,setActualSection]=useState('front')
@@ -27,16 +31,23 @@ export default function ServicesEN({darkmode}){
                     <div className={style.ArticleContainer}>
                        <article className={style.FrontServices}>
                         <h5>Creacion de paginas web de manera responsiva</h5>
-                        <picture></picture>
+                        <img src={imgfrontserv1} alt="serv1" />
                     </article>
                     <article className={style.FrontServices}>
-                        <h5>Desarrollo web de paginas estaticas y dinamica</h5>
+                        <h5>Desarrollo web de paginas estaticas</h5>
+                        <img src={imgfrontserv2} alt="serv2" />
+                    </article>
+                    <article className={style.FrontServices}>
+                        <h5>Desarrollo web de paginas dinamicas</h5>
+                        <img src={imgfrontserv3} alt="serv3" />
                     </article>
                     <article className={style.FrontServices}>
                        <h5>Desarrollo de aplicaciones web.</h5> 
+                       <img src={imgfrontserv4} alt="serv4" />
                     </article>
                     <article className={style.FrontServices}>
                        <h5>Desarrollo de UI/UX.</h5> 
+                       <img src={imgfrontserv5} alt='serv5' />
                     </article>
                      
                     </div>
@@ -53,23 +64,6 @@ export default function ServicesEN({darkmode}){
                 </section>}
 
             </div>
-                  <div>
-                  <picture>
-
-  <source srcset={imgfrontserv1} media="(max-width: 480px)"/>
-  
-
-  <source srcset={imgfrontserv1}media="(max-width: 800px)"/>
-  
- 
-  <source srcset={imgfrontserv1}/>
-  
- 
-  <img src={imgfrontserv1} alt="Imagen de ejemplo"/>
-</picture>
-<hr />
-<img src={imgfrontserv1} alt="" />
-                  </div>
         </div>
     )
 }
